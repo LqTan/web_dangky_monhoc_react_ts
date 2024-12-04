fc89f13fc5b9c03cb0f719c92239f51ade5093bc
tan.le <tan.le@kyanon.digital>
2024-12-04 21:58:41 +0700

    Deisng header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,24 +1,36 @@
 import React from 'react'
 import '../../styles/components/header/Header.css'
 import { Link } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa' // Thêm icon search
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
+    <nav className="navbar">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           TTTH DHSP TPHCM
         </Link>
+
+        <div className="search-container">
+          <div className="search-box">
+            <FaSearch className="search-icon" />
+            <input 
+              type="text" 
+              className="search-input" 
+              placeholder="Tra cứu..." 
+              style={{WebkitAppearance: 'none'}}
+            />
+          </div>
+        </div>
         
         <div className="navbar-links">
-          <Link to="/search" className="nav-link">
-            TRA CỨU
-          </Link>
           <Link to="/all-courses" className="nav-link">
-            TẤT CẢ KHÓA HỌC
+            <i className="fas fa-book-open"></i>
+            Tất cả môn học
           </Link>
           <Link to="/student-info" className="nav-link">
-            THÔNG TIN HỌC VIÊN
+            <i className="fas fa-user"></i>
+            Thông tin học viên
           </Link>
         </div>
       </div>
